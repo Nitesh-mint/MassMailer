dc90ea3295c7ef9326249bec422ab3b403bb604f
Niteh-mint <nitesh.raya058@gmail.com>
2023-06-25 11:31:03 +0545

    updated for dep

--- massmailer/settings.py
+++ massmailer/settings.py
@@ -137,7 +137,7 @@ import os
 STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [os.path.join(BASE_DIR,"static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"staticfiles")]
 STORAGES = {
     # ...
     "staticfiles": {
