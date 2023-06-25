35e782642e1f173a4b75dd7d2f6f89e586382502
Niteh-mint <nitesh.raya058@gmail.com>
2023-06-25 07:11:50 +0545

    removed sqlite

--- massmailer/settings.py
+++ massmailer/settings.py
@@ -88,12 +88,12 @@ WSGI_APPLICATION = 'massmailer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
