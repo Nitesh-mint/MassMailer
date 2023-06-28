5bc1b9223f485f4df98a5e8cf15cbca2c6b92660
Niteh-mint <nitesh.raya058@gmail.com>
2023-06-28 20:30:35 +0545

    update

--- massmailer/settings.py
+++ massmailer/settings.py
@@ -136,8 +136,7 @@ import os
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = BASE_DIR / 'staticfiles_build'
 
 
 # Default primary key field type
