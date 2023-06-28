7e39a51182375e9a2bb1dd5ddc2e40512d8ab003
Niteh-mint <nitesh.raya058@gmail.com>
2023-06-28 20:23:30 +0545

    initial commit

--- massmailer/settings.py
+++ massmailer/settings.py
@@ -136,7 +136,7 @@ import os
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+# STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
